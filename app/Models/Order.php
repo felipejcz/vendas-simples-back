0a1cc162530db79af391cc00966792c87d00f278
Felipe Zamboni <felipejcz@gmail.com>
2020-12-04 19:04:36 -0300

    Adicionado função de relação com 'customer' e função para somar o preço total da venda.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -13,8 +13,22 @@ class Order extends Model
 
     protected $fillable = ['customer_id', 'status', 'description'];
 
+    protected $appends = ['amount'];
+
     public function customer()
     {
-        return $this->hasOne(Customer::class);
+        return $this->hasOne(Customer::class, 'id', 'customer_id');
+    }
+
+    public function getAmountAttribute()
+    {
+        $list = OrderItems::where('order_id', $this->id)->get();
+        $list->load('product');
+        $amount = 0;
+
+        foreach ($list as $item) {
+            $amount += $item->product->price * $item->quantity;
+        }
+        return $amount;
     }
 }
